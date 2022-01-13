e23237bd783fc9852591bf109a6fcf2acbeebc14
杨 周 <1570571499@qq.com>
2022-01-13 10:46:01 +0800

    change project architecture

---
+++ src/Docnet.Core.Bindings/Bindings/DocumentWrapper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Docnet.Core.Exceptions;
+
+namespace Docnet.Core.Bindings
+{
+    internal sealed class DocumentWrapper : IDisposable
+    {
+        private readonly IntPtr _ptr;
+
+        public FpdfDocumentT Instance { get; private set; }
+
+        public DocumentWrapper(string filePath, string password)
+        {
+            Instance = fpdf_view.FPDF_LoadDocument(filePath, password);
+
+            if (Instance == null)
+            {
+                throw new DocnetLoadDocumentException("unable to open the document", fpdf_view.FPDF_GetLastError());
+            }
+        }
+
+        public DocumentWrapper(byte[] bytes, string password)
+        {
+            _ptr = Marshal.AllocHGlobal(bytes.Length);
+
+            Marshal.Copy(bytes, 0, _ptr, bytes.Length);
+
+            Instance = fpdf_view.FPDF_LoadMemDocument(_ptr, bytes.Length, password);
+
+            if (Instance == null)
+            {
+                throw new DocnetLoadDocumentException("unable to open the document", fpdf_view.FPDF_GetLastError());
+            }
+        }
+
+        public DocumentWrapper(FpdfDocumentT instance)
+        {
+            Instance = instance;
+
+            if (Instance == null)
+            {
+                throw new DocnetLoadDocumentException("unable to open the document");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (Instance == null)
+            {
+                return;
+            }
+
+            fpdf_view.FPDF_CloseDocument(Instance);
+
+            Marshal.FreeHGlobal(_ptr);
+
+            Instance = null;
+        }
+    }
+}
---
+++ src/Docnet.Core.iOS/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Docnet.Core.iOS
+{
+    public class Class1
+    {
+    }
+}
